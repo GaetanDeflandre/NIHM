f4d4cbc733f6ff2777d55937dd377d4da245f2f6
Gaetan Deflandre <gaetan.deflandre@etudiant.univ-lille1.fr>
2015-12-09 12:06:45 +0100

    fonction resample en cours

--- TPMarkov/src/Canvas2D.java
+++ TPMarkov/src/Canvas2D.java
@@ -9,70 +9,76 @@ import java.util.Vector;
 
 /**
  * Canvas2D.java
- *
+ * 
  * @author <a href="mailto:gery.casiez@lifl.fr">Gery Casiez</a>
  * @version
  */
 
-
-public class Canvas2D extends Canvas implements MouseMotionListener, MouseListener {
+public class Canvas2D extends Canvas implements MouseMotionListener,
+		MouseListener {
+	
 	// Stroke to test
 	private Vector<PointData> TStroke;
-	
+
 	// The points of TStroke resampled
 	private Vector<Point> TresampledPoints;
-	
+
 	// Hidden Markov Model
 	private HMM hmm;
-	
+
 	// Holds results to display on the interface
 	private Vector<String> console;
-	
-	Canvas2D () {
+
+	Canvas2D() {
 		TStroke = new Vector<PointData>();
 		TresampledPoints = new Vector<Point>();
-		
+
 		addMouseListener(this);
 		addMouseMotionListener(this);
-		
-		setBackground(new Color(255,255,255));
+
+		setBackground(new Color(255, 255, 255));
 		hmm = new HMM();
 
 		console = new Vector<String>();
 	}
-	
-	
-	public void paint (Graphics g){ 
-		g.drawString("Drag avec le bouton gauche ou droit de la souris : crŽation d'une courbe de test",10,15); 
-		int r = 5;  
-		
+
+	public void paint(Graphics g) {
+		g.drawString(
+				"Drag avec le bouton gauche ou droit de la souris : crï¿½ation d'une courbe de test",
+				10, 15);
+		int r = 5;
+
 		// Display the stroke
 		if (!TStroke.isEmpty()) {
 			g.setColor(Color.black);
 			for (int i = 1; i < TStroke.size(); i++) {
-				g.drawLine(TStroke.get(i-1).getPoint().x, TStroke.get(i-1).getPoint().y,
-						TStroke.get(i).getPoint().x, TStroke.get(i).getPoint().y);
-				g.drawArc(TStroke.get(i-1).getPoint().x - r,
-						TStroke.get(i-1).getPoint().y - r, 2*r, 2*r, 0, 360);
+				g.drawLine(TStroke.get(i - 1).getPoint().x, TStroke.get(i - 1)
+						.getPoint().y, TStroke.get(i).getPoint().x, TStroke
+						.get(i).getPoint().y);
+				g.drawArc(TStroke.get(i - 1).getPoint().x - r,
+						TStroke.get(i - 1).getPoint().y - r, 2 * r, 2 * r, 0,
+						360);
 			}
-			g.drawArc(TStroke.get(TStroke.size()-1).getPoint().x - r,
-					TStroke.get(TStroke.size()-1).getPoint().y - r, 2*r, 2*r, 0, 360);
+			g.drawArc(TStroke.get(TStroke.size() - 1).getPoint().x - r, TStroke
+					.get(TStroke.size() - 1).getPoint().y - r, 2 * r, 2 * r, 0,
+					360);
 		}
-		
+
 		// Display the console
 		g.setColor(Color.LIGHT_GRAY);
 		for (int i = 0; i < console.size(); i++) {
-			g.drawString(console.get(i),10,15*(i+2));			
+			g.drawString(console.get(i), 10, 15 * (i + 2));
 		}
-	
+
 		// Display the resampled stroke
 		if (!TresampledPoints.isEmpty()) {
 			g.setColor(Color.red);
 			for (int i = 0; i < TresampledPoints.size(); i++) {
-				g.drawArc(TresampledPoints.get(i).x - r, TresampledPoints.get(i).y - r, 2*r, 2*r, 0, 360);
+				g.drawArc(TresampledPoints.get(i).x - r,
+						TresampledPoints.get(i).y - r, 2 * r, 2 * r, 0, 360);
 			}
-		}		
-		
+		}
+
 	}
 
 	public void mouseMoved(MouseEvent e) {
@@ -82,18 +88,18 @@ public class Canvas2D extends Canvas implements MouseMotionListener, MouseListen
 	public void mouseDragged(MouseEvent e) {
 		TStroke.add(new PointData(e.getPoint(), e.getWhen()));
 		repaint();
-	}	
-	
+	}
+
 	public void mouseClicked(MouseEvent e) {
-		
+
 	}
 
 	public void mouseEntered(MouseEvent e) {
-		
+
 	}
 
 	public void mouseExited(MouseEvent e) {
-		
+
 	}
 
 	public void mousePressed(MouseEvent e) {
@@ -107,8 +113,8 @@ public class Canvas2D extends Canvas implements MouseMotionListener, MouseListen
 			TresampledPoints = hmm.getResampledPoints();
 			hmm.recognize();
 			console = hmm.getRecognitionInfo();
-			//hmm.TestAllExamples();
+			// hmm.TestAllExamples();
 			repaint();
 		}
-	}  
+	}
 }
--- TPMarkov/src/HMM.java
+++ TPMarkov/src/HMM.java
@@ -19,251 +19,282 @@ public class HMM {
 	private double score = 0;
 	private String nameTemplateFound = "none";
 	private Vector<Point> resampledRawPoints;
-	
-    /**
-     * List all the gestures classes (name of the templates)
-     */
-	Vector<String> gestureClasses;
-	
+
 	/**
-	* Hash map that gathers all the information on a class
-	*/
+	 * List all the gestures classes (name of the templates)
+	 */
+	Vector<String> gestureClasses;
+
+	/**
+	 * Hash map that gathers all the information on a class
+	 */
 	HashMap<String, GestureClass> classMap;
-	
+
 	TemplateManager templateManager;
-	
+
 	Vector<GestureProbability> gesturesProbabilities;
-	
-	
-	int cpt=0;
+
+	int cpt = 0;
 	int resamplingPeriod = 20;
-	
-	HMM () {
+
+	HMM() {
 		gestureClasses = new Vector<String>();
 		classMap = new HashMap<String, GestureClass>();
 		templateManager = new TemplateManager("gestures.xml");
 		gesturesProbabilities = new Vector<GestureProbability>();
-		//Training();
+		// Training();
 	}
-	
+
 	/**
 	 * Training step
 	 */
 	public void Training() {
 		// templates : list of all the templates of each class
 		Vector<Template> templates = templateManager.getTemplates();
-		
+
 		// Computes the features for each example (template)
-		for (int i=0; i<templates.size();i++) {
-			templates.get(i).setFeatures(computeFeatures(resample(templates.get(i).getPoints(),resamplingPeriod)));
+		for (int i = 0; i < templates.size(); i++) {
+			templates.get(i).setFeatures(
+					computeFeatures(resample(templates.get(i).getPoints(),
+							resamplingPeriod)));
 		}
-		
+
 		// gestureClasses : list of all the gesture classes
-		for (int i=0; i<templates.size();i++) gestureClasses.add(templates.get(i).getName());
+		for (int i = 0; i < templates.size(); i++)
+			gestureClasses.add(templates.get(i).getName());
 		Collections.sort(gestureClasses);
 		// Remove duplicates
 		int i = 1;
-		while (i<gestureClasses.size()) {
-			if (gestureClasses.get(i).compareTo(gestureClasses.get(i-1)) == 0) gestureClasses.remove(i);
-			else i++;
+		while (i < gestureClasses.size()) {
+			if (gestureClasses.get(i).compareTo(gestureClasses.get(i - 1)) == 0)
+				gestureClasses.remove(i);
+			else
+				i++;
 		}
-		
+
 		System.out.println("Liste des classes : " + gestureClasses.toString());
-		
+
 		// Gather the templates
-		for (i=0; i<gestureClasses.size();i++) {
+		for (i = 0; i < gestureClasses.size(); i++) {
 			String className = gestureClasses.get(i);
 			Vector<Template> classExamples = new Vector<Template>();
-			for (int j=0; j<templates.size();j++) if (templates.get(j).getName().compareTo(className) == 0) classExamples.add(templates.get(j));
-			GestureClass gestureClass = new GestureClass(classExamples, className);
-			classMap.put(className, gestureClass);	
+			for (int j = 0; j < templates.size(); j++)
+				if (templates.get(j).getName().compareTo(className) == 0)
+					classExamples.add(templates.get(j));
+			GestureClass gestureClass = new GestureClass(classExamples,
+					className);
+			classMap.put(className, gestureClass);
 		}
-		
-		//gestureClasses.remove("arrow");
-		//gestureClasses.remove("leftCurlyBrace");
-		//gestureClasses.remove("pigtail");
-		//gestureClasses.remove("rightCurlyBrace");
-		//System.out.println("Liste des classes : " + gestureClasses.toString());
-		
+
+		// gestureClasses.remove("arrow");
+		// gestureClasses.remove("leftCurlyBrace");
+		// gestureClasses.remove("pigtail");
+		// gestureClasses.remove("rightCurlyBrace");
+		// System.out.println("Liste des classes : " +
+		// gestureClasses.toString());
+
 		// KMeansLearner
-		for (int c=0; c<gestureClasses.size();c++) {
+		for (int c = 0; c < gestureClasses.size(); c++) {
 			classMap.get(gestureClasses.get(c)).computeKmeansLearner();
 		}
-		
+
 		// Print hmm for each gesture class
 		/*
-		for (int c=0; c<gestureClasses.size();c++) {
-			try {
-				(new GenericHmmDrawerDot()).write(classMap.get(gestureClasses.get(c)).getHMM(), gestureClasses.get(c)+".dot");
-				Runtime.getRuntime().exec("/usr/local/bin/dot -Tpdf " + gestureClasses.get(c)+".dot" + " -o " +  gestureClasses.get(c)+".pdf");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}		
-		try {
-			Runtime.getRuntime().exec("shutdown -r -t 1 " );
-			} catch (IOException t) { }
-		*/
+		 * for (int c=0; c<gestureClasses.size();c++) { try { (new
+		 * GenericHmmDrawerDot
+		 * ()).write(classMap.get(gestureClasses.get(c)).getHMM(),
+		 * gestureClasses.get(c)+".dot");
+		 * Runtime.getRuntime().exec("/usr/local/bin/dot -Tpdf " +
+		 * gestureClasses.get(c)+".dot" + " -o " +
+		 * gestureClasses.get(c)+".pdf"); } catch (IOException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); } } try {
+		 * Runtime.getRuntime().exec("shutdown -r -t 1 " ); } catch (IOException
+		 * t) { }
+		 */
 	}
-	
-	public void recognize() {	
-	
+
+	public void recognize() {
+
 		gesturesProbabilities.clear();
-		
-		if (rawSrcPoints.size() < 4) return;
-		ArrayList<Double> featuresRawPoints = computeFeatures(resample(rawSrcPoints,resamplingPeriod));
+
+		if (rawSrcPoints.size() < 4)
+			return;
+		ArrayList<Double> featuresRawPoints = computeFeatures(resample(
+				rawSrcPoints, resamplingPeriod));
 		score = Double.MIN_VALUE;
 		nameTemplateFound = "none";
-		for (int c=0; c<gestureClasses.size();c++) {
-			double scoreClass = classMap.get(gestureClasses.get(c)).computeScore(featuresRawPoints);
-			//System.out.println(gestureClasses.get(c) + " " + scoreClass);
-			gesturesProbabilities.add(new GestureProbability(gestureClasses.get(c), scoreClass));
+		for (int c = 0; c < gestureClasses.size(); c++) {
+			double scoreClass = classMap.get(gestureClasses.get(c))
+					.computeScore(featuresRawPoints);
+			// System.out.println(gestureClasses.get(c) + " " + scoreClass);
+			gesturesProbabilities.add(new GestureProbability(gestureClasses
+					.get(c), scoreClass));
 			if (scoreClass > score) {
 				score = scoreClass;
 				nameTemplateFound = gestureClasses.get(c);
 			}
 		}
 		Collections.sort(gesturesProbabilities);
-		
-		
-		//System.out.println("Classe = " + nameTemplateFound + " " + score);
-		
-		
+
+		// System.out.println("Classe = " + nameTemplateFound + " " + score);
+
 	}
-	
+
 	public Vector<String> getRecognitionInfo() {
 		Vector<String> res = new Vector<String>();
 		int cpt = 1;
-		for (int i=0; i<gesturesProbabilities.size(); i++) {
+		for (int i = 0; i < gesturesProbabilities.size(); i++) {
 			if (gesturesProbabilities.get(i).getPi() > 0) {
-				res.add(cpt + ". " + gesturesProbabilities.get(i).getName() + " " + gesturesProbabilities.get(i).getPi());
+				res.add(cpt + ". " + gesturesProbabilities.get(i).getName()
+						+ " " + gesturesProbabilities.get(i).getPi());
 				cpt++;
 			}
 		}
-		if (nameTemplateFound.compareTo("none")!=0) {
-			Vector<String> obsVectors = classMap.get(nameTemplateFound).getObservationVectors();
-			
+		if (nameTemplateFound.compareTo("none") != 0) {
+			Vector<String> obsVectors = classMap.get(nameTemplateFound)
+					.getObservationVectors();
+
 			res.add("");
 			res.add("Sequence d'observation:");
 			DecimalFormat format = new DecimalFormat("#0");
 			format.setMinimumIntegerDigits(2);
-			ArrayList<Double> featuresRawPoints = computeFeatures(resample(rawSrcPoints,resamplingPeriod));
+			ArrayList<Double> featuresRawPoints = computeFeatures(resample(
+					rawSrcPoints, resamplingPeriod));
 			String tmp = "";
 			for (Double i : featuresRawPoints) {
 				tmp += format.format(i.intValue()) + " ";
 			}
 			res.add(tmp);
-			
+
 			res.add("");
-			res.add("Sequences d'observations pour le geste "+ nameTemplateFound +":");
+			res.add("Sequences d'observations pour le geste "
+					+ nameTemplateFound + ":");
 			res.addAll(obsVectors);
 		}
-		
-		return res; 
+
+		return res;
 	}
-	
+
 	public double getScore() {
 		return score;
-	}	
-	
+	}
+
 	public String getNameTemplateFound() {
 		return nameTemplateFound;
-	}	
+	}
 
 	public void setRawSourcePoints(Vector<PointData> rawPoints) {
-		writeRawPoints2XMLFile("mer",rawSrcPoints);
+		writeRawPoints2XMLFile("mer", rawSrcPoints);
 		cpt++;
 		System.out.println(cpt);
-		
+
 		rawSrcPoints = rawPoints;
-		resampledRawPoints = resample(rawPoints,resamplingPeriod);
-	}	
-	
+		resampledRawPoints = resample(rawPoints, resamplingPeriod);
+	}
+
 	public void TestAllExamples() {
-		int cpt=0;
-		int good =0;
-		for (int c=0; c<gestureClasses.size();c++) {
+		int cpt = 0;
+		int good = 0;
+		for (int c = 0; c < gestureClasses.size(); c++) {
 			GestureClass gestClass = classMap.get(gestureClasses.get(c));
-			for (int i=0; i< gestClass.getNumberExamples();i++) {
+			for (int i = 0; i < gestClass.getNumberExamples(); i++) {
 				rawSrcPoints = gestClass.examples.get(i).getPoints();
 				recognize();
 				if (gestureClasses.get(c).compareTo(getNameTemplateFound()) == 0)
 					good++;
 				else
-					System.out.println("Bad - " + gestureClasses.get(c) + " example num " + i);
+					System.out.println("Bad - " + gestureClasses.get(c)
+							+ " example num " + i);
 				cpt++;
 			}
 		}
-		System.out.println("Recognition rate of examples = " + good/(cpt*1.0));
+		System.out.println("Recognition rate of examples = " + good
+				/ (cpt * 1.0));
 	}
-	
-	
-	/**
-	 * Compute features 
-	 */
-	
-	public ArrayList<Double> computeFeatures(Vector<Point> points) {
-		
-		return null;
-	}
-	
 
 	/**
-	 * Add new gestures to out.xml XML file. Then copy and paste the data in out.xml file to gestures.xml file
+	 * Compute features
+	 */
+
+	public ArrayList<Double> computeFeatures(Vector<Point> points) {
+
+		return null;
+	}
+
+	/**
+	 * Add new gestures to out.xml XML file. Then copy and paste the data in
+	 * out.xml file to gestures.xml file
+	 * 
 	 * @param points
 	 */
 	public void writeRawPoints2XMLFile(String name, Vector<PointData> points) {
 		try {
-		FileWriter fstream = new FileWriter("out.xml", true);
-		BufferedWriter out = new BufferedWriter(fstream);
-		out.write("	<template name=\"" + name + "\" nbPts=\"" + points.size() +"\">\n");
-		for (int i=0; i<points.size();i++) {
-			out.write("		<Point x=\"" + points.get(i).getPoint().x + "\" y=\"" +
-					points.get(i).getPoint().y + "\" ts=\"" + points.get(i).getTimeStamp() +"\"/>\n");
-			//if (i<points.size()-1) System.out.print(",");
+			FileWriter fstream = new FileWriter("out.xml", true);
+			BufferedWriter out = new BufferedWriter(fstream);
+			out.write("	<template name=\"" + name + "\" nbPts=\""
+					+ points.size() + "\">\n");
+			for (int i = 0; i < points.size(); i++) {
+				out.write("		<Point x=\"" + points.get(i).getPoint().x
+						+ "\" y=\"" + points.get(i).getPoint().y + "\" ts=\""
+						+ points.get(i).getTimeStamp() + "\"/>\n");
+				// if (i<points.size()-1) System.out.print(",");
+			}
+
+			out.write("	</template>\n");
+			out.close();
+		} catch (Exception e) {// Catch exception if any
+			System.err.println("Error: " + e.getMessage());
 		}
-		
-		out.write("	</template>\n");
-		out.close();
-		} catch (Exception e){//Catch exception if any
-		      System.err.println("Error: " + e.getMessage());
-	    }
 
-	}	
-	
-
+	}
 
 	/**
 	 * Distance between two points
+	 * 
 	 * @param p0
 	 * @param p1
 	 * @return
 	 */
-	public double distance (Point p0, Point p1) {
-		return Math.sqrt((p1.x - p0.x) * (p1.x - p0.x) + (p1.y - p0.y) * (p1.y - p0.y));
-	}	
-	
-	public double squareDistance (Point p0, Point p1) {
+	public double distance(Point p0, Point p1) {
+		return Math.sqrt((p1.x - p0.x) * (p1.x - p0.x) + (p1.y - p0.y)
+				* (p1.y - p0.y));
+	}
+
+	public double squareDistance(Point p0, Point p1) {
 		return (p1.x - p0.x) * (p1.x - p0.x) + (p1.y - p0.y) * (p1.y - p0.y);
-	}		
+	}
 
 	/**
 	 * Resample points to have one point each deltaTms ms
+	 * 
 	 * @param p0
 	 * @param p1
 	 * @return
-	 */	
-	
+	 */
 	protected Vector<Point> resample(Vector<PointData> pts, int deltaTms) {
 		Vector<Point> res = new Vector<Point>();
-		
+
+		long totalTime = pts.lastElement().getTimeStamp()
+				- pts.firstElement().getTimeStamp();
+
+		long requiredTime = deltaTms;
+		long t0 = pts.firstElement().getTimeStamp();
+
+		int i = 1;
+
+		res.add(pts.firstElement().getPoint());
+
+		while (requiredTime < totalTime) {
+			
+			// voir fonction de sam sur slack
+
+		}
+
 		return res;
 	}
-	
+
 	public Vector<Point> getResampledPoints() {
 		return resampledRawPoints;
 	}
 
-	
 }
