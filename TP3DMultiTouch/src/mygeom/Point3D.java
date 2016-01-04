e427ae7cb0e7163341cae2b86a170962221de502
Gaëtan DEFLANDRE <gaetan.deflandre@gmail.com>
2016-01-04 03:15:11 +0100

    ajout de la class Point3D

---
+++ TP3DMultiTouch/src/mygeom/Point3D.java
@@ -0,0 +1,20 @@
+package mygeom;
+
+public class Point3D {
+	
+	public double x;
+	public double y;
+	public double z;
+	
+	public Point3D() {
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+	
+	public Point3D(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
